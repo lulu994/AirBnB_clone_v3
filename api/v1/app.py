837c469210cc1f497a7f8221985a138796a41db4
lulu994 <loubna.neddah@gmail.com>
2023-10-31 11:47:19 +0100

    adding files

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""Flask server (variable app)
 """
-
-
-from flask import Flask, jsonify
-from models import storage
-from os import getenv
+module
+"""
 from api.v1.views import app_views
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+from models import storage
+import os
+
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
+def teardown(self):
+    """
+    closing function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Not found'), 404
+    """
+    404 error page
+    """
+    not_found = {'error': 'Not found'}
+    return jsonify(not_found)
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    host_ip = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    host_port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host_ip, port=host_port)
